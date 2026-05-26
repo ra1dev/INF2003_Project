489bc73965afdcece7ea5f8341758885869112e6
Amos Ng <75406378+fatmosthegreat@users.noreply.github.com>
2026-05-26 13:30:05 +0800

    Add files via upload

---
+++ Backend/db_conn.py
@@ -0,0 +1,21 @@
+import psycopg2
+from flask import g
+
+def get_db():
+    if "db" not in g:
+        g.db = psycopg2.connect(
+            host="localhost",
+            dbname="epl_db",
+            user="postgres",
+            password="password",
+            port=5432
+        )
+    return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
